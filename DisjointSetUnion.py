56ddc4423b52c913071118fa2be7cf9586ad9518
Tatiana <10772992+Vikora@users.noreply.github.com>
2023-04-29 10:20:48 +0300

    Update and rename DisjointSet.py to DisjointSetUnion.py

--- DisjointSetUnion.py
+++ DisjointSetUnion.py
@@ -1,26 +1,26 @@
-class DisjointSet:
+class DisjointSetUnion:
     def __init__(self, size):
-        self._root = list(range(size))
+        self._parent = list(range(size))
         self._rank = [0] * size
 
     def find(self, x):
         # if x isn't a root
-        if self._root[x] != x:
+        if self._parent[x] != x:
             # find a root of its parent recursively
-            self._root[x] = self.find(self._root[x])  
-        return self._root[x]
+            self._parent[x] = self.find(self._parent[x])  
+        return self._parent[x]
 
     def union(self, x, y):
         rootX = self.find(x)
         rootY = self.find(y)
         if rootX != rootY:
             if self._rank[rootX] > self._rank[rootY]:
-                self._root[rootY] = rootX
+                self._parent[rootY] = rootX
             elif self._rank[rootX] < self._rank[rootY]:
-                self._root[rootX] = rootY
+                self._parent[rootX] = rootY
             else:
-                self._root[rootX] = rootY
+                self._parent[rootX] = rootY
                 self._rank[rootX] += 1
 
-    def isConnected(self, x, y):
+    def areConnected(self, x, y):
         return self.find(x) == self.find(y)
